0b8771aab67f3d4eebb73b822d645696aae8356f
Alex Barth <alex@developmentseed.org>
2013-07-20 16:04:01 -0400

    Update download script with unzip and download paths.

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-
+curl -L "http://dcatlas.dcgis.dc.gov/catalog/download.asp?downloadID=1021&downloadTYPE=ESRI" -o BldgPly.zip
+curl -L "http://dcatlas.dcgis.dc.gov/catalog/download.asp?downloadID=2182&downloadTYPE=ESRI" -o AddressPt.zip
+unzip BldgPly.zip -d BldgPly
+unzip AddressPt.zip -d AddressPt
 ogr2ogr -t_srs EPSG:3857 BldgPly/buildings.shp BldgPly/BldgPly.shp
 ogr2ogr -t_srs EPSG:3857 AddressPt/addresses.shp AddressPt/AddressPt.shp
-
