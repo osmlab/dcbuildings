8ac695209129f905d71cf4bbaa395e59b4b8f380
Alex Barth <alex@developmentseed.org>
2013-07-20 16:35:11 -0400

    First working output, creates a valid OSM file with first building and first address.

--- convert.py
+++ convert.py
@@ -10,13 +10,46 @@ from lxml.etree import tostring
 # create XML 
 results = etree.Element('osm', version='0.6', generator='alex@mapbox.com')
 
+ids = dict(
+    node = -1,
+    way = -1,
+    rel = -1
+)
+def newId(type):
+    ids[type] = ids[type] - 1
+    return ids[type]
+
 with collection("AddressPt/addresses.shp", "r") as input:
     for address in input:
-        pprint(address)
+        # pprint(address)
         node = etree.Element('node',
-            lat=str(address['geometry']['coordinates'][0]),
-            lon=str(address['geometry']['coordinates'][0]))
+            lon=str(address['geometry']['coordinates'][0]),
+            lat=str(address['geometry']['coordinates'][1]),
+            visible='true',
+            id=str(newId('node')))
+        node.append(etree.Element('tag',
+            k='addr:housenumber',
+            v=str(address['properties']['ADDRNUM'])));
         results.append(node)
+        break
+
+with collection("BldgPly/buildings.shp", "r") as input:
+    for building in input:
+        # pprint(building)
+        way = etree.Element('way',
+            visible='true',
+            id=str(newId('way')))
+        way.append(etree.Element('tag', k='building', v='yes'));
+        for pos in building['geometry']['coordinates'][0]:
+            id = str(newId('node'))
+            node = etree.Element('node',
+                lon=str(pos[0]),
+                lat=str(pos[1]),
+                visible='true',
+                id=id)
+            results.append(node)
+            way.append(etree.Element('nd', ref=id))
+        results.append(way)
         break
 
 handle = open('buildings.osm', 'w')
@@ -24,9 +57,3 @@ handle.writelines(tostring(results, pretty_print=True, xml_declaration=True, enc
 handle.close()
 
 exit(0)
-
-with collection("BldgPly/buildings.shp", "r") as input:
-    for building in input:
-        pprint(building)
-        break
-
